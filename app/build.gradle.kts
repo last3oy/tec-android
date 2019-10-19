5bebc8dab6fefdac2711ad5888c9960d2a65e073
last3oy <softwarez_gelion@hotmail.com>
2019-10-19 15:08:17 +0700

    #11 : Update dependencies and remove dagger (temporary)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 }
 
 android {
-    compileSdkVersion(28)
+    compileSdkVersion(29)
 
     defaultConfig {
         applicationId = "com.last3oy.tecandroid"
         minSdkVersion(21)
-        targetSdkVersion(28)
+        targetSdkVersion(29)
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -38,26 +38,27 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
     // kotlin
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.11")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.0.1")
 
     // android
-    implementation("androidx.appcompat:appcompat:1.1.0-alpha02")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.appcompat:appcompat-resources:1.1.0")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.1.0-alpha02")
+    implementation("androidx.recyclerview:recyclerview:1.1.0-beta05")
     implementation("androidx.palette:palette:1.0.0")
-    implementation("com.google.android.material:material:1.1.0-alpha03")
+    implementation("com.google.android.material:material:1.1.0-beta01")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel:2.1.0-alpha02")
-    implementation("androidx.lifecycle:lifecycle-livedata:2.1.0-alpha02")
-    implementation("androidx.room:room-runtime:2.1.0-alpha04")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.2.0-beta01")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.2.0-beta01")
+    implementation("androidx.room:room-runtime:2.2.0")
 
-    kapt("androidx.lifecycle:lifecycle-compiler:2.1.0-alpha02")
-    kapt("androidx.room:room-compiler:2.1.0-alpha04")
+    kapt("androidx.lifecycle:lifecycle-compiler:2.2.0-beta01")
+    kapt("androidx.room:room-compiler:2.2.0")
 
     // retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.4.0")
+    implementation("com.squareup.retrofit2:retrofit:2.6.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.4.0")
     implementation("com.squareup.okhttp3:logging-interceptor:3.9.1")
     implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-experimental-adapter:1.0.0")
@@ -68,21 +69,21 @@ dependencies {
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.6.0")
 
     // dagger
-    implementation("com.google.dagger:dagger:2.16")
-    implementation("com.google.dagger:dagger-android-support:2.16")
-
-    kapt("com.google.dagger:dagger-compiler:2.16")
-    kapt("com.google.dagger:dagger-android-processor:2.16")
+//    implementation("com.google.dagger:dagger:2.24")
+//    implementation("com.google.dagger:dagger-android-support:2.24")
+//
+//    kapt("com.google.dagger:dagger-compiler:2.16")
+//    kapt("com.google.dagger:dagger-android-processor:2.16")
 
     // picasso
     implementation("com.squareup.picasso:picasso:3.0.0-SNAPSHOT")
 
     // unit test
     testImplementation("junit:junit:4.12")
-    testImplementation("androidx.arch.core:core-testing:2.0.0")
-    testImplementation("androidx.room:room-testing:2.1.0-alpha04")
+    testImplementation("androidx.arch.core:core-testing:2.1.0")
+    testImplementation("androidx.room:room-testing:2.2.0")
 
     // instrumented test
-    androidTestImplementation("androidx.test:runner:1.1.2-alpha01")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.2-alpha01")
+    androidTestImplementation("androidx.test:runner:1.3.0-alpha02")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0-alpha02")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,13 @@
 buildscript {
     repositories {
         google()
+        jcenter()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.0-beta04")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.3.21")
+        classpath("com.android.tools.build:gradle:3.6.0-beta01")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.3.50")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
